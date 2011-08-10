b1c33b29fd33ff7e347786bf589a73dd30aca064
Jared 「ジャッカル」Spencer <jklz@4pi.us>
2011-08-09 19:24:55 -0600

    Working example
    This is a really simple example use of the iShip API using javascript as the client and simply displays the price that the shipment was quoted at. There is more information that you can show but i felt for a start returning a price was plenty.
    
    this example code for a javascript connection into the iShip API uses code from
    https://github.com/douglascrockford/JSON-js/

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function httpGet(theUrl)
+    {
+    var xmlHttp = null;
+	
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+    }
